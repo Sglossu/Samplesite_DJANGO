e20613523644d631ae68c009fb7127106bbe723a
Sglossu <Alexandra@Alexandras-MacBook-Air.local>
2022-05-07 22:16:44 +0400

    контроллер-класс

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import index, by_rubric
+from .views import index, by_rubric, BbCreateView
 
 urlpatterns = [
+    # маршрутр, указвыающий на контроллер
+    path('add/', BbCreateView.as_view(), name='add'),
     path('<int:rubric_id>/', by_rubric),
     path('', index),
 ]
